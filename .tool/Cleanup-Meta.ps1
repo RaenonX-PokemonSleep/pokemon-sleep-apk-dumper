ecd65c95a9f9c955d12e92320f75d33f18d00356
RaenonX <raenonx0710@gmail.com>
2024-09-24 18:44:46 +0800

    Updated dumper tool to `Cpp2IL` #4
    closes #4

--- .tool/Cleanup-Meta.ps1
+++ .tool/Cleanup-Meta.ps1
@@ -10,45 +10,9 @@ param(
 )
 
 $ReplaceTargetRegexList = @(
-    # [Token(Token = "0x4002C36")]
-    '.*\[Token\(Token = "0x.*"\)\]\r\n'
-    # # [Address(RVA = "0x44E8F94", Offset = "0x44E8F94", VA = "0x44E8F94")]
-    '.*\[Address\(RVA = "0x.*", Offset = "0x.*", VA = "0x.*"\)\]\r\n'
-    # [CompilerGenerated]
-    '.*\[CompilerGenerated\]\r\n'
-    # [DebuggerHidden]
-    '.*\[DebuggerHidden\]\r\n'
-    # [SerializeField]
-    '.*\[SerializeField\]\r\n'
-    # [SyncField]
-    '.*\[SyncField\]\r\n'
-	# [SyncList(DataType = typeof(UserMainDreamGiftDataUnit), AlwaysInsert = true)]
-    '.*\[SyncList.*\]\r\n'
-    # [SyncBinder]
-    '.*\[SyncBinder\]\r\n'
-    # [SyncListIdentifier]
-    # [SyncListIdentifier(IsPrimaryKey = true)]
-    '.*\[SyncListIdentifier.*\]\r\n'
-    # [SyncBindedIdentifier]
-    '.*\[SyncBindedIdentifier\]\r\n'
-    # [IgnoreDataMember]
-    '.*\[IgnoreDataMember\]\r\n'
-    # [FieldOffset(Offset = "0x60")]
-    '.*\[FieldOffset\(Offset = "0x.*"\)\]\r\n'
-    # [StructLayout(3)]
-    '.*\[StructLayout\(.*\)\]\r\n'
-    # [Il2CppDummyDll.FieldOffset(Offset = "0x0")]
-    '.*\[Il2CppDummyDll.FieldOffset\(Offset = "0x.*"\)\]\r\n'
-    # [Obsolete("UIMaterial component is deprecated. Please use UIMaterialValue instead.")]
-    '.*\[Obsolete\(".*"\)\]\r\n'
-    # [Tooltip("Line mode only")]
-    '.*\[Tooltip\(".*"\)\]\r\n'
-    # [RequireComponent(typeof(DraggableCamera))]
-    '.*\[RequireComponent\(".*"\)\]\r\n'
-    # [Space(5f)]
-    '.*\[Space\(.*\)\]\r\n'
-    # [DisallowMultipleComponent]
-    '.*\[DisallowMultipleComponent\]\r\n'
+    #  //Field offset: 0x0
+    # > Note the space before //
+    '//Field offset: .*'
 )
 
 function Format-File {
